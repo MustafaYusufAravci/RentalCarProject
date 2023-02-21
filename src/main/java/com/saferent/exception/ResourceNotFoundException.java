ec0bb2182391c7497ff46ae079a6a7394df62d0d
Mustafa Yusuf Aravci <musti.19287@gmail.com>
2023-02-21 23:56:01 +0300

    Day7

--- src/main/java/com/saferent/exception/ResourceNotFoundException.java
+++ src/main/java/com/saferent/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.saferent.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
@@ -9,4 +9,5 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
         super(message);
     }
+
 }
